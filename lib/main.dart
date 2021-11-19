18cd04cb3a9e99385d180d151886981486a17bbd
Frazile Inc <Frazile.com@gmail.com>
2021-11-19 22:16:50 +0530

    updated with BLOCCONSUMER

--- lib/main.dart
+++ lib/main.dart
@@ -41,62 +41,60 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: BlocListener<CounterCubit, CounterState>(
-        listener: (context, state) {
-          if (state.wasIncremented!) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(
-                content: Text('Incremented'),
-                duration: Duration(milliseconds: 300),
-              ),
-            );
-          } else if (state.wasIncremented == false) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(
-                content: Text('Decremented'),
-                duration: Duration(milliseconds: 300),
-              ),
-            );
-          }
-        },
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'You have pushed the button this many times:',
-              ),
-              BlocBuilder<CounterCubit, CounterState>(
-                builder: (context, state) {
-                  return Text(
-                    state.counterValue.toString(),
-                    style: Theme.of(context).textTheme.headline4,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            BlocConsumer<CounterCubit, CounterState>(
+              listener: (context, state) {
+                if (state.wasIncremented!) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('Incremented'),
+                      duration: Duration(milliseconds: 300),
+                    ),
                   );
-                },
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  FloatingActionButton(
-                    onPressed: () =>
-                        BlocProvider.of<CounterCubit>(context).decrement(),
-                    tooltip: 'Decrement',
-                    child: const Icon(
-                      Icons.remove,
+                } else if (state.wasIncremented == false) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('Decremented'),
+                      duration: Duration(milliseconds: 300),
                     ),
+                  );
+                }
+              },
+              builder: (context, state) {
+                return Text(
+                  state.counterValue.toString(),
+                  style: Theme.of(context).textTheme.headline4,
+                );
+              },
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                FloatingActionButton(
+                  onPressed: () =>
+                      BlocProvider.of<CounterCubit>(context).decrement(),
+                  tooltip: 'Decrement',
+                  child: const Icon(
+                    Icons.remove,
                   ),
-                  FloatingActionButton(
-                    onPressed: () =>
-                        BlocProvider.of<CounterCubit>(context).increment(),
-                    tooltip: 'Increment',
-                    child: const Icon(
-                      Icons.add,
-                    ),
+                ),
+                FloatingActionButton(
+                  onPressed: () =>
+                      BlocProvider.of<CounterCubit>(context).increment(),
+                  tooltip: 'Increment',
+                  child: const Icon(
+                    Icons.add,
                   ),
-                ],
-              ),
-            ],
-          ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
