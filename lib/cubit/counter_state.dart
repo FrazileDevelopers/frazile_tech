6e02de0b0ef373423688496c2af2f53ae7e3298f
Frazile Inc <Frazile.com@gmail.com>
2021-11-20 04:40:49 +0530

    Bloc Testing updated

--- lib/cubit/counter_state.dart
+++ lib/cubit/counter_state.dart
@@ -1,11 +1,26 @@
 part of 'counter_cubit.dart';
 
-class CounterState {
+class CounterState extends Equatable {
   int? counterValue;
   bool? wasIncremented;
 
   CounterState({
-    this.counterValue,
+    @required this.counterValue,
     this.wasIncremented,
   });
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is CounterState &&
+        other.counterValue == counterValue &&
+        other.wasIncremented == wasIncremented;
+  }
+
+  @override
+  int get hashCode => counterValue.hashCode ^ wasIncremented.hashCode;
+
+  @override
+  List<Object?> get props => [counterValue, wasIncremented];
 }
---
+++ test/cubit/counter_cubit_test.dart
@@ -0,0 +1,46 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:fraziletech/cubit/counter_cubit.dart';
+import 'package:test/expect.dart';
+import 'package:test/scaffolding.dart';
+
+void main() {
+  group('CounterCubit', () {
+    CounterCubit? counterCubit;
+    setUp(() {
+      counterCubit = CounterCubit();
+    });
+    tearDown(() {
+      counterCubit!.close();
+    });
+
+    test(
+        'the initial state for the CounterCubit is CounterState(counterValue:0)',
+        () {
+      expect(counterCubit!.state, CounterState(counterValue: 0));
+    });
+
+    blocTest<CounterCubit, CounterState>(
+      'the cubit should emit a CounterState(counterValue: 5, wasIncremented:true) when cubit.increment() function is called ',
+      build: () => counterCubit!,
+      act: (cubit) => cubit.increment(),
+      expect: () => <CounterState>[
+        CounterState(
+          counterValue: 5,
+          wasIncremented: true,
+        ),
+      ],
+    );
+
+    blocTest<CounterCubit, CounterState>(
+      'the cubit should emit a CounterState(counterValue: -5, wasIncremented:true) when cubit.increment() function is called ',
+      build: () => counterCubit!,
+      act: (cubit) => cubit.decrement(),
+      expect: () => <CounterState>[
+        CounterState(
+          counterValue: -5,
+          wasIncremented: false,
+        ),
+      ],
+    );
+  });
+}
